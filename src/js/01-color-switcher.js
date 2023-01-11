512bc3ad8e1579493a8c3bf0b9df29df2bce3572
Viktor Oliinyk <kanoute1992@gmail.com>
2023-01-11 11:37:08 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+startBtn.addEventListener('click', startBtnClick);
+stopBtn.addEventListener('click', stopBtnClick);
+
+function startBtnClick() {
+  stopBtn.removeAttribute('disabled');
+  startBtn.setAttribute('disabled', 'disabled');
+  changeBgColor();
+};
+
+function stopBtnClick() {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(intervalId);
+};
+
+function changeBgColor() {
+  return intervalId = setInterval(() => {
+      body.style.backgroundColor =  getRandomHexColor();
+  }, 1000)
+};
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
